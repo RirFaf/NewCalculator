a0f979a210f623b50ba63706aa5f59da7e24108e
Rinat <mylittlebigplanet24@gmail.com>
2022-09-22 22:24:46 +0300

    Copied previous project

---
+++ app/src/main/java/com/example/newcalculator/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.newcalculator
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    fun oneBTNAction(view: View) {
+
+    }
+
+    fun twoBTNAction(view: View) {
+
+    }
+
+    fun threeBTNAction(view: View) {
+
+    }
+
+    fun fourBTNAction(view: View) {
+
+    }
+
+    fun fiveBTNAction(view: View) {
+
+    }
+
+    fun sixBTNAction(view: View) {
+
+    }
+
+    fun sevenBTNAction(view: View) {
+
+    }
+
+    fun eightBTNAction(view: View) {
+
+    }
+
+    fun nineBTNAction(view: View) {
+
+    }
+
+    fun zeroBTNAction(view: View) {
+
+    }
+
+    fun addBTNAction(view: View) {
+
+    }
+
+    fun subtractBTNAction(view: View) {
+
+    }
+
+    fun multiplyBTNAction(view: View) {
+
+    }
+
+    fun divideBTNAction(view: View) {
+
+    }
+
+    fun resultBTNAction(view: View) {
+
+    }
+
+    fun clearBTNAction(view: View) {
+
+    }
+}
